78cdb67fbee364c7001bb48cc3e0db9224442775
snail007 <arraykeys@gmail.com>
2015-11-20 16:48:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tar zxfv jdk-8u5-linux-x64.tar.gz 
 tar zxfv lib.tar.gz
-mv lib jdk1.8.0_05/
+mv lib jdk1.8.0_05/lib
 mv jdk1.8.0_05 /usr/local/
 cp jdk.sh /etc/profile.d/jdk.sh
 chmod +x /etc/profile.d/jdk.sh
